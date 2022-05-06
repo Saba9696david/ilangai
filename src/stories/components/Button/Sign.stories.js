02369eb92d47c80722e556e2dc8a00e2017df9d3
Saba9696david <saba@thillaitech.com>
2022-05-06 15:33:49 +0530

    update button components and create icons

--- src/stories/components/Button/Sign.stories.js
+++ src/stories/components/Button/Sign.stories.js
@@ -1,5 +1,6 @@
 import React from 'react' 
 import Button from './Sign'
+import FacebookIcon from '@material-ui/icons/Facebook'
 
 export default {
     title: 'SignIn & SignOut/Button'
@@ -8,3 +9,4 @@ export default {
 export const SignIn = () => <Button variant='signin'>Sign In</Button>
 export const SignUp = () => <Button variant='signup'>Sign Up</Button>
 export const Submit = () => <Button variant='submit'>Submit</Button>
+export const Facebook = () => <Button variant='fb'>&nbsp;<FacebookIcon></FacebookIcon>&nbsp;&nbsp;&nbsp;&nbsp;Sign in with Facebook</Button>
---
+++ src/stories/components/Icons/Icons.stories.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+import FacebookIcon from '@material-ui/icons/Facebook'
+import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
+import ChatIcon from '@material-ui/icons/Chat';
+
+export default {
+    title: 'Icons/Symbols'
+}
+
+export const FB = () => <h1><FacebookIcon></FacebookIcon></h1>
+export const Voice = () => <h1><KeyboardVoiceIcon></KeyboardVoiceIcon></h1>
+export const chat = () => <h1><ChatIcon></ChatIcon></h1>
